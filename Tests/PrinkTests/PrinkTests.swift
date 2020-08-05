e1aa13f8e3d2db26745a588fa4b90eede78acf99
daybysay <yukita.sekai+github@gmail.com>
2020-08-05 10:57:10 +0900

    ...

--- Tests/PrinkTests/PrinkTests.swift
+++ Tests/PrinkTests/PrinkTests.swift
@@ -1,15 +1,29 @@
 import XCTest
+import LinkPresentation
 @testable import Prink
 
 final class PrinkTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Prink().text, "Hello, World!")
+    func testOnMemoryRepositoryMetadataDefaultNil() {
+        let repository: PrinkRepository = OnMemoryRepository()
+        let result =  repository.metadata(url: URL(string: "https://example.com")!)
+        XCTAssertEqual(result, nil)
+    }
+    
+    func testOnMemoryRepositoryMetadataStore() {
+        let repository: PrinkRepository = OnMemoryRepository()
+        let metadata = LPLinkMetadata()
+        let url = URL(string: "https://example.com")!
+        metadata.url = url
+        
+        let b = repository.store(metadata: metadata)
+        XCTAssertTrue(b)
+        
+        let result =  repository.metadata(url: url)
+        XCTAssertEqual(result, metadata)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testOnMemoryRepositoryMetadataDefaultNil", testOnMemoryRepositoryMetadataDefaultNil),
+        ("testOnMemoryRepositoryMetadataStore", testOnMemoryRepositoryMetadataStore),
     ]
 }
